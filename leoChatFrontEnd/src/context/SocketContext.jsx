ce8f235d01dba9b9c93bf0fc60c04d99c1c63db5
anbuleo <anbuliyon@gmail.com>
2024-03-05 17:15:20 +0530

    url

--- leoChatFrontEnd/src/context/SocketContext.jsx
+++ leoChatFrontEnd/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({children})=>{
     const  {authUser} = useAuthContext()
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:8000",{
+            const socket = io("https://leochatapp.onrender.com",{
                 query : {
                     userId: authUser._id
                 }
