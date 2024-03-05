e266ab6aa9dfb828cc164e9e9218ea3c5baa1879
anbuleo <anbuliyon@gmail.com>
2024-03-05 17:02:51 +0530

    url done

--- leoChatFrontEnd/src/Pages/Login.jsx
+++ leoChatFrontEnd/src/Pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import useLogin from "../hooks/useLogin"
 
 
@@ -8,6 +8,7 @@ function Login() {
     const [username,setUsername] = useState("")
     const [password,setPassword] = useState("")
     const {loading, login} = useLogin()
+    let navigate = useNavigate()
 
     const handleSubmit = async(e)=>{
         e.preventDefault()
@@ -32,9 +33,9 @@ function Login() {
                 </label>
                 <input value={password}  onChange={(e)=>setPassword(e.target.value)} type="password" placeholder='Enter Password' className='bg-transparent input input-bordered text-orange-100 input-info w-full max-w-xs h-10' />
             </div>
-            <Link to={'signup'}>
+            <div onClick={()=>navigate('/signup')}>
                 <p className="text-sm text-green-950 hover:underline hover:text-orange-600 mt-2 inline-block">{"Don't" } have an account?</p>
-            </Link>
+            </div>
             <div className="">
                 <button className="btn btn-block btn-outline btn-info btn-sm mt-2  " disabled={loading} >{loading ? (<span className=" text-orange-100 loading loading-spinner"></span>): "Login" }</button>
             </div>
