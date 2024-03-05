b547d7fe31a85e22975ad15c11ddcd8eb677ea6f
anbuleo <anbuliyon@gmail.com>
2024-03-05 16:41:10 +0530

    deployment

--- LeoChatBackend/index.js
+++ LeoChatBackend/index.js
@@ -1,9 +1,12 @@
+import path from 'path'
 import express from "express";
 import dotenv from 'dotenv'
 import AppRoutes from './routes/index.js'
 import cookieParser from "cookie-parser";
 // import connetToDatabase from "./models/index.js";
 import { app, server } from "./socket/socket.js";
+
+const __dirname = path.resolve()
 
 dotenv.config()
 
@@ -13,6 +16,11 @@ let PORT = process.env.PORT||5000
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api',AppRoutes)
+
+app.use(express.static(path.join(__dirname,"/leoChatFrontEnd/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"leoChatFrontEnd","dist","index.html"))
+})
 
 
 server.listen(PORT,()=>{
