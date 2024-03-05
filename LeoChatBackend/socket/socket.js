a9826c1487e9645f5030ce1ac6c3a4bf79dca7e9
anbuleo <anbuliyon@gmail.com>
2024-03-05 16:53:38 +0530

    deployment

--- LeoChatBackend/socket/socket.js
+++ LeoChatBackend/socket/socket.js
@@ -7,7 +7,7 @@ let app =express()
 const server = http.createServer(app)
 const io = new Server(server,{
     cors: {
-        origin: ['http://localhost:3000'],
+        origin: ['https://leochatapp.onrender.com'],
         methods: ['GET','POST']
     }
 })
